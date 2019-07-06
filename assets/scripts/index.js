3d7b624446a25a2c2b60409978f2051b8af7ae42
kevinrosario <kevin.rosario9@upr.edu>
2019-07-06 01:38:09 -0400

    Finished with Skills section
    1. Added remaining skills.
    2. Styled skills sections.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,32 @@
 $(() => {
+  $('.front-end-gallery').flickity({
+    contain: true,
+    freeScroll: true,
+    pageDots: false,
+    wrapAround: true
+    // autoPlay: true
+  })
+  $('.back-end-gallery').flickity({
+    contain: true,
+    freeScroll: true,
+    pageDots: false,
+    wrapAround: true
+    // autoPlay: true
+  })
 
+  $('.miscellaneous-gallery').flickity({
+    contain: true,
+    freeScroll: true,
+    pageDots: false,
+    wrapAround: true
+    // autoPlay: true
+  })
+
+  $('.programming-gallery').flickity({
+    contain: true,
+    freeScroll: true,
+    pageDots: false,
+    wrapAround: true
+    // autoPlay: true
+  })
 })
