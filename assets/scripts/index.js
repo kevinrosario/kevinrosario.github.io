3e23d45cee80dd4d7f3430b5fb7029d44fd12353
kevinrosario <kevin.rosario9@upr.edu>
2019-07-15 21:01:23 -0400

    Added projects pictures
    1. Added projects pictures.
    2. Styled pictures.
    3. Added text overlay.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -25,4 +25,29 @@ $(() => {
     pageDots: false,
     cellAlign: 'left'
   })
+
+  // Clear all forms
+  $('#reset-forms').on('click', event => {
+    $('.form-control').val('')
+  })
+
+  // Send email
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const name = $('#customer-name').val()
+    const email = $('#customer-email').val()
+    const message = $('#customer-messsage').val()
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:7165/send',
+      data: {
+        name: name,
+        email: email,
+        messsage: message
+      }
+    }).then((response) => {
+      console.log(response)
+    }).catch(console.error)
+  }
+  $('#contact-form').on('submit', handleSubmit)
 })
