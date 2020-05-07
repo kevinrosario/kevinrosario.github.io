44c66b77de1ae37b1ad71fd58aa665240b576422
kevinrosario <kevin.rosario9@upr.edu>
2020-05-06 23:26:10 -0400

    Updates

---
+++ component---src-pages-index-js-874281224b0ebcbfde10.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),c=t.n(a),r=t("Bl7J"),l=t("vrFN");n.default=function(){return c.a.createElement(r.a,null,c.a.createElement(l.a,{title:"Home"}),c.a.createElement("h1",null,"Under construction"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-874281224b0ebcbfde10.js.map
